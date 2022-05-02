c68372df1155e7e31ed0331ba5fd311865ae8456
davydebut <spanudavy@gmail.com>
2022-05-02 09:52:07 +0200

    modif var en let pour jquery

--- Projet_En_Construction/js/script.js
+++ Projet_En_Construction/js/script.js
@@ -2,7 +2,7 @@ $(function () {
     $('#contact-form').submit(function (e) { // cette fonction est executée lorsque le formulaire est soumis
         e.preventDefault(); // permet d'enlever le comportement par défaut du formulaire
         $('.comments').empty(); // vide le contenu de la div .comments (les commentaires)
-        var postData = $('#contact-form').serialize(); // récupère les données du formulaire et le met dans une variable postData
+        let postData = $('#contact-form').serialize(); // récupère les données du formulaire et le met dans une variable postData
         $.ajax({  // envoie la variable postData au fichier script.php objet json ajax
             type: 'POST', // type de requête
             url: 'php/contact.php', // url du fichier script.php
